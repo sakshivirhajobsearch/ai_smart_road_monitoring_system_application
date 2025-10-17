76fb5ea0bf457c0b0934608f4eee88cf98997039
anuragvasubharti <anuragvasubharti@gmail.com>
2025-10-17 18:24:10 +0530

    Added

--- database/schema.sql
+++ database/schema.sql
@@ -5,11 +5,13 @@ USE ai_smart_road_monitoring_system_application;
 -- -------------------------
 -- Table structure for users
 -- -------------------------
+-- Users table
 CREATE TABLE IF NOT EXISTS users (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    role VARCHAR(50) NOT NULL
+    role VARCHAR(50) NOT NULL,
+    enabled BOOLEAN DEFAULT TRUE
 );
 
 -- -------------------------
