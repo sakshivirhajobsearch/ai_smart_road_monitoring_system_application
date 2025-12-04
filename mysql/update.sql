b1c4396b19f2bba706a57865dad1a4c93875f1c6
anuragvasubharti <anuragvasubharti@gmail.com>
2025-12-04 15:14:35 +0530

    Added

--- mysql/update.sql
+++ mysql/update.sql
@@ -1,16 +1,1 @@
-USE ai_smart_road_monitoring_system_application;
-
--- UPDATE USER PASSWORD
-UPDATE user SET password = 'newPassword' WHERE username = 'user';
-
--- UPDATE ROAD CONDITION
-UPDATE road_data SET `condition` = 'GOOD'
-WHERE id = 2;
-
--- UPDATE POTHOLE STATUS
-UPDATE pothole SET severity = 'LOW'
-WHERE id = 1;
-
--- UPDATE REPAIR ACTIVITY
-UPDATE repair_activity SET status = 'COMPLETED'
-WHERE id = 2;
+-- UPDATE QUERIES HERE
